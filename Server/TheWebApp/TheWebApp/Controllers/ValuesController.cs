c52801a81cbe9ad64990e4504b0a3b232991a38d
alex <amccloy@gmail.com>
2018-10-20 14:02:01 +1000

    why are you reading this

--- Server/TheWebApp/TheWebApp/Controllers/ValuesController.cs
+++ Server/TheWebApp/TheWebApp/Controllers/ValuesController.cs
@@ -1,14 +1,20 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.SQLite;
+using System.IO;
 using System.Linq;
 using System.Net;
 using System.Net.Http;
+using System.Text;
+using System.Web.Hosting;
 using System.Web.Http;
 
 namespace TheWebApp.Controllers
 {
     public class ValuesController : ApiController
     {
+        private SQLiteConnection dbConnection;
+
         // GET api/values
         public IEnumerable<string> Get()
         {
@@ -18,7 +24,22 @@ namespace TheWebApp.Controllers
         // GET api/values/5
         public string Get(int id)
         {
-            return "value";
+            using (dbConnection = new SQLiteConnection($"Data Source = {HostingEnvironment.MapPath(@"~/App_Data/test.db")}.; Version = 3;"))
+            {
+                dbConnection.Open();
+                string query = "SELECT * FROM testTable";
+                var command = new SQLiteCommand(query, dbConnection);
+                var reader = command.ExecuteReader();
+
+                StringBuilder sb = new StringBuilder();
+
+                while(reader.Read())
+                {
+                    sb.Append($"Col1: {reader["col1"]}, Col2: {reader["col2"]}");
+                }
+
+                return sb.ToString();
+            }
         }
 
         // POST api/values
